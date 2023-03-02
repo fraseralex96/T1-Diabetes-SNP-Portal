4d15b14a27e84b3a897fd36075d5be9ff851e4d3
fraseralex96 <73662925+fraseralex96@users.noreply.github.com>
2023-03-02 15:23:09 +0000

    Add files via upload

--- final_website/app.py
+++ final_website/app.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-#create a flask instance
-app = Flask(__name__)
-#configure the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/DB.db'
+app = Flask(__name__) # create a flask instance
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/DB.db' # configure the SQLAlchemy database our DB.db SQLite database
+
 
 #create an instance of the SQLalchemy class and associate it with our flask app
 db = SQLAlchemy(app)
-db.init_app(app)
+db.init_app(app) # initialise the flask app
 
 
 
