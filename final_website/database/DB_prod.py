d67ce9615d0f5422cb704abccf3cbdc59d89e6aa
fraseralex96 <73662925+fraseralex96@users.noreply.github.com>
2023-03-02 15:37:23 +0000

    Add files via upload

--- final_website/database/DB_prod.py
+++ final_website/database/DB_prod.py
@@ -3,17 +3,20 @@ import csv, sqlite3
 import pandas as pd
 
 #create a connection to a SQLite database
-con = sqlite3.connect('DB.db')
+con = sqlite3.connect('DB.db') # creates a DB.db if one is not found
 cur = con.cursor() 
+
+############VARIANTS###############################
 
 #SQL to add data to the Variants table
 cur.execute("CREATE TABLE Variants(Variant_ID INT PRIMARY KEY, Variant_name VARCHAR(30), Allele VARCHAR(30), P_value REAL, Chromosome INT, Location_on_chromosome INT);")
 
-with open('./Data/main.csv') as file:
+with open('./Data/main.csv') as file: # commit a row in the SQL table for every row in the file
     for row in file:
-        cur.execute("INSERT INTO Variants VALUES(?,?,?,?,?,?)", row.split(','))
+        cur.execute("INSERT INTO Variants VALUES(?,?,?,?,?,?)", row.split(',')) # takes into account the files are csv
         con.commit()
 
+############CLINICAL SIGNIFICANCE###################
 
 #SQL to create and add data to clinical significance table
 cur.execute("CREATE TABLE Clinical_significance(Variant_ID INT PRIMARY KEY, Variant_name VARCHAR(30), Variant_consequence TEXT, Clinical_significance TEXT, polyPhen_prediction TEXT, polyPhen_score INT, sift_prediction TEXT, sift_score INT, FOREIGN KEY (Variant_name) REFERENCES Variants(Variant_name));")
@@ -23,9 +26,9 @@ with open('./Data/cs.csv') as file:
         cur.execute("INSERT INTO Clinical_significance VALUES(?,?,?,?,?,?,?,?)", row.split(','))
         con.commit()
 
+############RAF#####################################
 
 #SQL to create and add data to RAFs table 
-
 cur.execute("CREATE TABLE RAFs(Variant_ID INT PRIMARY KEY, Variant_name VARCHAR(30), Han_raf FLOAT, Yoruba_raf FLOAT, GBR_raf FLOAT, FOREIGN KEY (Variant_name) REFERENCES Variants(Variant_name));")
 
 with open('./Data/raf.csv') as file:
@@ -33,32 +36,36 @@ with open('./Data/raf.csv') as file:
         cur.execute("INSERT INTO RAFs VALUES(?,?,?,?,?)", row.split(','))
         con.commit()
 
+############VARIANT GENE RELATIONSHIP###############
 
 #SQL to create and add data to Variant_gene_relationship table 
-
 cur.execute("CREATE TABLE Variant_gene_relationship(Variant_name VARCHAR(30) NOT NULL, Gene_name VARCHAR(30) NOT NULL, PRIMARY KEY (Variant_name, Gene_name), FOREIGN KEY (Variant_name) REFERENCES Variants(Variant_name));")
 
 
 with open('./Data/variant_gene_pairings.csv') as file:
     for row in file:
         # Trim the gene name before inserting it into the table
-        gene_name = row.split(',')[1].strip()  # strip() removes leading/trailing whitespace
+        gene_name = row.split(',')[1].strip()  # strip() removes leading/trailing whitespace which was present in this csv
         # Check if the record already exists
         cur.execute("SELECT * FROM Variant_gene_relationship WHERE Variant_name=? AND Gene_name=?", (row.split(',')[0], gene_name))
         existing_record = cur.fetchone()
 
-        # Insert the record only if it doesn't already exist
+        # Insert the record only if it doesn't already exist as there were issues with duplicated entries
         if not existing_record:
             cur.execute("INSERT INTO Variant_gene_relationship VALUES(?,?)", (row.split(',')[0], gene_name))
             con.commit()
-#SQL to create and add data to GO_terms table 
 
+############GO TERMS#################################
+
+#SQL to create and add data to GO_terms table 
 cur.execute("CREATE TABLE GO_terms(Gene_ID INT PRIMARY KEY, Gene_name VARCHAR(30), GO_domain TEXT, GO_term_name TEXT, GO_term_definition TEXT, GO_term_accession TEXT,FOREIGN KEY (Gene_name) REFERENCES Variant_gene_relationship(Gene_name));")
 
 with open('./Data/gt.tsv') as file:
     for row in file:
         cur.execute("INSERT INTO GO_terms VALUES(?,?,?,?,?,?)", row.split('\t'))
         con.commit()
+
+############LD MATRICES#####################################
 
 #create a matrix for LD values for GBR chr6
 
@@ -67,13 +74,14 @@ df = pd.read_csv('./Data/chr6_GBR_r.csv')
 cols = df.columns.tolist()
 cols.pop(0)
 
-cur.execute("CREATE TABLE chr6_GBR_r(rs_value VARCHAR(30) PRIMARY KEY);")
+cur.execute("CREATE TABLE chr6_GBR_r(rs_value VARCHAR(30) PRIMARY KEY);") # start by creating empty table
+
 #add a column for each of the column names in df, importantly this is automated
 for col in cols:
     cur.execute(f"ALTER TABLE chr6_GBR_r ADD COLUMN {col} FLOAT")
     con.commit()
 
-with open('./Data/chr6_GBR_r.csv') as file:
+with open('./Data/chr6_GBR_r.csv') as file: # then add the data to the columns
     for row in file:
         cur.execute("INSERT INTO chr6_GBR_r VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", row.split(','))
         con.commit()
@@ -114,4 +122,4 @@ with open('./Data/chr6_Yoruba_r.csv') as file:
         cur.execute("INSERT INTO chr6_Yoruba_r VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", row.split(','))
         con.commit()
 
-con.close()
+con.close() # must close connection!
